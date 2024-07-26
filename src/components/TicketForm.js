45c61090c5a81d33f1ab31bdc765624a1b2d103c
Evan McKinney-Robinson <evan.mckinney-robinson@theverygroup.com>
2024-07-26 11:51:13 +0100

    Update TicketForm

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -18,5 +18,49 @@ export default function TicketForm() {
         setPriority('1');
     }
 
-    return(<></>)
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        clearForm();
+
+        console.log('Title:', title);
+        console.log('Description:', description);
+        console.log('Priority:', priority);
+        clearForm();
+    }   
+
+    return (
+        <form onSubmit={handleSubmit} className="ticket-form">
+            <div>
+                <label>Title</label>
+                <input type="text" value={title} className="form-input" onChange={e => setTitle(e.target.value)}></input>
+            </div>
+            <div>
+                <label>Description</label>
+                <textarea type="text" value={title} className="form-input" onChange={e => setDescription(e.target.value)}></textarea>
+            </div>
+
+            <fieldset className="priority-fieldset">
+                <legend>Priority</legend>
+
+                {
+                    Object.entries(priorityLabels).map(([value, label]) => (
+                        <label key={value} className="priority-label">
+                            <input 
+                            type="radio" 
+                            value={value} 
+                            checked={priority === value}
+                            className="priority-input"
+                            onChange={(e) => setPriority(e.target.value)}
+                            ></input>
+                            {label}
+                        </label>
+                    ))
+                }
+            </fieldset>
+
+            <button type="submit" className="button">Submit</button>
+
+        </form>
+    );
 }
